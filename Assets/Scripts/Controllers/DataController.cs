192e8383fadef409ce4b6c0baac8d6678a0d8224
yosonlineapp <yos.online.app@gmail.com>
2020-06-21 10:09:31 +0900

    Crystal and UI added

---
+++ Assets/Scripts/Controllers/DataController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DataController : MonoBehaviour
+{
+  public int crystalCount = 0;
+  private TextMeshProUGUI crystalUI;
+  void Start()
+  {
+    crystalUI = GameObject.Find("CrystalCount").GetComponent<TextMeshProUGUI>();
+    DontDestroyOnLoad(this.gameObject);
+  }
+  public void addCrystal(int getCrystal)
+  {
+    crystalCount += getCrystal;
+    crystalUI.text = crystalCount.ToString();
+  }
+}
---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+  public GameObject dataController;
+  void Start()
+  {
+    if (!GameObject.Find("DataController"))
+    {
+      GameObject dataObject = Instantiate(dataController) as GameObject;
+      dataObject.name = "DataController";
+    }
+  }
+}
